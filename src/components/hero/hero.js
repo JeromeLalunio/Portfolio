7434ce4823bbe44bd74210ad0d10e2b09d5c5467
Jerome Lalunio <josejeromelalunio@gmail.com>
2021-01-30 06:59:27 +0800

    finished hero

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,14 +3,12 @@ import background from '../../assets/background.png';
 
 export default function Hero(){
     return <main className = 'hero-main'>
-        <img src={background} alt = 'background'/>
+        <img className = 'hero-background' src={background} alt = 'background'/>
         <div className = 'hero-text'>
             <p className = 'hello'>Hello I'm</p>
             <h1 className = 'name'>Jerome Lalunio</h1>
-            <p>And this is my Portfolio</p>
-            <br />
-            <br />
-            <br />
+            <p className = 'hero-message'>I'm a Web and App developer</p>
+            <br /><br /><br /><br /><br /><br />
         </div>
     </main>
 }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,11 +2,10 @@ import './navbar.scss';
 import NavbarItem from './components/navbar_item';
 
 export default function NavBar() {
-    return <nav>
+    return <nav className = 'nav-bar'>
         <NavbarItem name = 'About'/>
         <NavbarItem name = 'Skills'/>
         <NavbarItem name = 'Portfolio'/>
         <NavbarItem name = 'Contact'/>
-
     </nav>
 }
